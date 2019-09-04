fc63382cd8c375022581fd7629b60fac5d0eb7d0
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2019-09-04 21:56:18 +0200

    Add the license to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright (c) 2014-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+
 # Heavily influenced by https://github.com/theopolis/build-anywhere
 
 function build_gcc() {
