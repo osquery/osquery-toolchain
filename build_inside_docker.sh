65a8942374e7b616c3729c2a082c65d70c463448
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2020-08-27 19:06:41 +0200

    Fix and update licenses for build scripts

--- build_inside_docker.sh
+++ build_inside_docker.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# Copyright (c) 2014-present, The osquery authors
+#
+# This source code is licensed as defined by the LICENSE file found in the
+# root directory of this source tree.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
 
 export CMAKE_VERSION="3.15.2"
 export BASE_IMAGE="ubuntu:bionic"
